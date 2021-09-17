fc7eedf4497eeaa8ce3cb5a4ae039a94a0d3b8ed
johnnygerard <jafgerard@outlook.com>
2021-09-17 06:08:12 +0200

    Release version 2.0

--- pwdgen.py
+++ pwdgen.py
@@ -22,38 +22,63 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from string import ascii_letters, digits, punctuation, ascii_lowercase, ascii_uppercase
+from string import digits, octdigits, punctuation, ascii_lowercase, ascii_uppercase
 from secrets import choice, randbelow
 import argparse, sys
 
 parser = argparse.ArgumentParser(prog='pwdgen', formatter_class=argparse.RawTextHelpFormatter, description=
     '''Generate offline secure passwords using a CSPRNG (Cryptographically Strong Pseudo Random Number Generator).
-Each character is randomly selected from the ASCII character set (excluding C0 control, space and DEL characters).
+ 
 
-By default, all characters are used (same as -z/--all).
-Options -e/--exclude and -i/--include override other options (surrounding quotes may be required).''', epilog=
+By default, each character is randomly selected from the ASCII character set (excluding space and control characters).
+The user-defined character set is built in two phases:
+
+    The first phase forms a base character set using one or more flags (--all when no flags are passed).
+    Each flag is its own character set and their combination defines a character superset (equivalent to their set union).
+
+    The second phase adds or substracts specific characters from that set using the options --include or --exclude.
+    These two options may require surrounding quotes and default to the empty string.''', epilog=
     """ 
-EXAMPLES (using default length except for the first):
+EXAMPLES
 
-  4-digit PIN:              pwdgen -d 4
-  no symbols:               pwdgen -a
-  only symbols:             pwdgen -s
-  default:                  pwdgen
-  no slashes:               pwdgen -e '\/'
-  only slashes and letters: pwdgen -Li '\/'""")
+  4-digit PIN:         pwdgen -d 4
+  no symbols:          pwdgen -a
+  symbols:             pwdgen -s
+  default:             pwdgen
+  no slashes:          pwdgen -e '\/'
+  slashes and letters: pwdgen -Li '\/'
+  8-bit key:           pwdgen -b 8
+  base64 key:          pwdgen -ai '+/'
+  a, b and c's:        pwdgen -0i abc""")
 parser.add_argument('length', nargs='?', default=16, type=int, help='number of password characters (default: 16)')
-parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 2.0', help='''show program's version number and exit
+ ''')
 
-parser.add_argument('-z', '--all',          action='store_true', help='all characters (default)')
 parser.add_argument('-l', '--lowercase',    action='store_true', help='latin small letters (a to z)')
 parser.add_argument('-u', '--uppercase',    action='store_true', help='latin capital letters (A to Z)')
-parser.add_argument('-L', '--letter',       action='store_true', help='same as -lu')
 parser.add_argument('-d', '--digit',        action='store_true', help='decimal digits (0 to 9)')
-parser.add_argument('-a', '--alphanumeric', action='store_true', help='same as -Ld')
 parser.add_argument('-s', '--symbol',       action='store_true', help='punctuation and symbols')
+parser.add_argument('-L', '--letter',       action='store_true', help='same as --lowercase --uppercase')
+parser.add_argument('-a', '--alphanumeric', action='store_true', help='same as --letter --digit')
+parser.add_argument('-A', '--all',          action='store_true', help='same as --alphanumeric --symbol (default)')
+parser.add_argument('-0', '--empty',        action='store_true', help='''empty character set (only useful if combined with --include)
+ ''')
 
 parser.add_argument('-e', '--exclude', default='', metavar='EXCLUDED', help='string of characters to exclude')
-parser.add_argument('-i', '--include', default='', metavar='INCLUDED', help='string of characters to include')
+parser.add_argument('-i', '--include', default='', metavar='INCLUDED', help='''string of characters to include
+ ''')
+
+parser.add_argument('-b', '--binary',       action='store_true', help='bits (0 | 1)')
+parser.add_argument('-o', '--octal',        action='store_true', help='octal digits (0 to 7)')
+parser.add_argument('-x', '--hex-lower',    action='store_true', help='lowercase hexadecimal digits (0 to 9 | a to f)')
+parser.add_argument('-X', '--hex-upper',    action='store_true', help='''uppercase hexadecimal digits (0 to 9 | A to F)
+ ''')
+
+parser.add_argument('--pure', action='store_true', help='''Disable the minimum of 1 character applied to the following categories:
+    digits, symbols, lowercase and uppercase
+This minimum only applies to passwords with a length of at least 4.
+As example: 'pwdgen 4' always contains exactly 1 character of each category.
+            'pwdgen 4 --pure' could yield the password 0000 or $$$$.''')
 
 namespace = parser.parse_args()
 
@@ -61,57 +86,91 @@ namespace = parser.parse_args()
 if namespace.length <= 0:
     sys.exit('pwdgen: error: length must be positive')
 
-# validate --exclude and --include arguments
+
+# define character sets
+LOWERCASE    = set(ascii_lowercase)
+UPPERCASE    = set(ascii_uppercase)
+LETTER       = LOWERCASE | UPPERCASE
+DIGIT        = set(digits)
+ALPHANUMERIC = LETTER | DIGIT
+SYMBOL       = set(punctuation)
+ALL          = ALPHANUMERIC | SYMBOL
+BINARY       = set('01')
+OCTAL        = set(octdigits)
+HEX_LOWER    = DIGIT | set('abcdef')
+HEX_UPPER    = DIGIT | set('ABCDEF')
+
+
 excluded_set = set(namespace.exclude)
 included_set = set(namespace.include)
+
+# sanitize --exclude and --include arguments
+for char in (excluded_set | included_set):
+    if char not in ALL:
+        sys.exit(f'pwdgen: error: found unauthorized character (U+{ord(char):04X})')
+
+# check --exclude and --include for conflicts
 if excluded_set & included_set:
-    sys.exit('pwdgen: error: conflict between --exclude and --include characters')
+    sys.exit('pwdgen: error: options --exclude and --include conflict (common characters disallowed)')
 
-# define 4-bit mask
-LOWERCASE    = 0b1000
-UPPERCASE    = 0b0100
-DIGIT        = 0b0010
-SYMBOL       = 0b0001
-
-LETTER       = LOWERCASE | UPPERCASE
-ALPHANUMERIC = LETTER | DIGIT
-ALL          = ALPHANUMERIC | SYMBOL
-
-
-bitmask = 0
+# phase 1: combine flags to build the base character set
+character_set = set()
 if namespace.all:
-    bitmask = ALL
+    character_set = ALL
 else:
-    if namespace.lowercase:    bitmask |= LOWERCASE
-    if namespace.uppercase:    bitmask |= UPPERCASE
-    if namespace.letter:       bitmask |= LETTER
-    if namespace.digit:        bitmask |= DIGIT
-    if namespace.alphanumeric: bitmask |= ALPHANUMERIC
-    if namespace.symbol:       bitmask |= SYMBOL
-    # use default flag (--all)
-    if bitmask == 0:           bitmask  = ALL
+    if namespace.lowercase:    character_set |= LOWERCASE
+    if namespace.uppercase:    character_set |= UPPERCASE
+    if namespace.letter:       character_set |= LETTER
+    if namespace.digit:        character_set |= DIGIT
+    if namespace.alphanumeric: character_set |= ALPHANUMERIC
+    if namespace.symbol:       character_set |= SYMBOL
+    if namespace.binary:       character_set |= BINARY
+    if namespace.octal:        character_set |= OCTAL
+    if namespace.hex_lower:    character_set |= HEX_LOWER
+    if namespace.hex_upper:    character_set |= HEX_UPPER
+    # default flag (--all) or --empty
+    if not character_set and not namespace.empty:
+        character_set = ALL
+
+# phase 2: add or substract using --include and --exclude strings
+character_set |= included_set
+character_set -= excluded_set
 
 
-ascii_lowercase = list(set(ascii_lowercase) - excluded_set if bitmask & LOWERCASE else set(ascii_lowercase) & included_set)
-ascii_uppercase = list(set(ascii_uppercase) - excluded_set if bitmask & UPPERCASE else set(ascii_uppercase) & included_set)
-digits          = list(set(digits)          - excluded_set if bitmask & DIGIT     else set(digits)          & included_set)
-punctuation     = list(set(punctuation)     - excluded_set if bitmask & SYMBOL    else set(punctuation)     & included_set)
-
-selected_characters = digits + punctuation + ascii_lowercase + ascii_uppercase
-if len(selected_characters) == 0:
+character_list = list(character_set)
+if not character_list:
     sys.exit('pwdgen: error: character set empty')
 password = []
 
-if namespace.length >= 4:
-    # make sure we have at least one character for each non-empty category
-    for chars in [digits, punctuation, ascii_lowercase, ascii_uppercase]:
-        if len(chars) > 0:
-            password.insert(randbelow(len(password) + 1), choice(chars))
-            namespace.length -= 1
+if namespace.length >= 4 and not namespace.pure:
+    # classify characters into 4 categories
+    DIGITS = []
+    PUNCTUATION = []
+    ASCII_LOWERCASE = []
+    ASCII_UPPERCASE = []
+    for char in character_list:
+        if   char in digits:          DIGITS.append(char)
+        elif char in ascii_lowercase: ASCII_LOWERCASE.append(char)
+        elif char in ascii_uppercase: ASCII_UPPERCASE.append(char)
+        else:                         PUNCTUATION.append(char)
+    
+    count = sum([bool(DIGITS),
+                 bool(PUNCTUATION),
+                 bool(ASCII_LOWERCASE),
+                 bool(ASCII_UPPERCASE)])
 
-# remaining characters
-for i in range(namespace.length):
-    password.insert(randbelow(len(password) + 1), choice(selected_characters))
+    for i in range(namespace.length - count):
+            password.append(choice(character_list))
+
+    # make sure we have at least one character for each non-empty category
+    for chars in [DIGITS, PUNCTUATION, ASCII_LOWERCASE, ASCII_UPPERCASE]:
+        if chars:
+            # insert instead of append to maintain randomness
+            password.insert(randbelow(len(password) + 1), choice(chars))
+else:
+    # remaining characters
+    for i in range(namespace.length):
+        password.append(choice(character_list))
 
 
 print(''.join(password))
